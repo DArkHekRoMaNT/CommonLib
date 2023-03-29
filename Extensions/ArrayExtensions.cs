f35eb55ea7df746887af5a47fd4f829525dcb300
DArkHekRoMaNT <darkhekromant@yandex.ru>
2023-03-29 10:52:58 +0300

    Add AppendIf from SQ

---
+++ Extensions/ArrayExtensions.cs
@@ -0,0 +1,35 @@
+using Vintagestory.API.Util;
+
+namespace CommonLib.Extensions
+{
+    public static class ArrayExtensions
+    {
+        /// <summary>
+        /// Creates a new copy of the array with value append to the end of the array
+        /// if condition is true
+        /// </summary>
+        public static T[] AppendIf<T>(this T[] array, bool condition, T value)
+        {
+            if (condition)
+            {
+                return array.Append(value);
+            }
+
+            return array;
+        }
+
+        /// <summary>
+        /// Creates a new copy of the array with value append to the end of the array
+        /// if condition is true
+        /// </summary>
+        public static T[] AppendIf<T>(this T[] array, bool condition, params T[] value)
+        {
+            if (condition)
+            {
+                return array.Append(value);
+            }
+
+            return array;
+        }
+    }
+}

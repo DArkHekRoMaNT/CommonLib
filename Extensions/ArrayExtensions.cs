002ddafc9e8d7280d6e0ded1a7f52d700b68d2b4
DArkHekRoMaNT <darkhekromant@yandex.ru>
2023-04-07 13:01:17 +0300

    Remove not needed AppendIf

--- Extensions/ArrayExtensions.cs
+++ Extensions/ArrayExtensions.cs
@@ -8,20 +8,6 @@ namespace CommonLib.Extensions
         /// Creates a new copy of the array with value append to the end of the array
         /// if condition is true
         /// </summary>
-        public static T[] AppendIf<T>(this T[] array, bool condition, T value)
-        {
-            if (condition)
-            {
-                return array.Append(value);
-            }
-
-            return array;
-        }
-
-        /// <summary>
-        /// Creates a new copy of the array with value append to the end of the array
-        /// if condition is true
-        /// </summary>
         public static T[] AppendIf<T>(this T[] array, bool condition, params T[] value)
         {
             if (condition)

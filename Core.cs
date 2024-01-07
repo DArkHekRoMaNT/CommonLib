afd552c8a4c701c8f18f4f5540a7de3e07b6e5ad
DArkHekRoMaNT <DArkHekRoMaNT@yandex.ru>
2024-01-07 04:32:10 +0300

    Fix no client /cl config

--- Core.cs
+++ Core.cs
@@ -6,7 +6,7 @@ namespace CommonLib
 {
     public class Core : ModSystem
     {
-        public override void StartServerSide(ICoreServerAPI api)
+        public override void Start(ICoreAPI api)
         {
             api.ChatCommands
                 .Create("cl")
@@ -14,6 +14,10 @@ namespace CommonLib
                 .WithDescription("CommonLib commands");
 
             ConfigCommand.Create(api, Mod.Logger);
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
             RandomTeleportCommand.Create(api, Mod.Logger);
             RestoreStabilityCommand.Create(api, Mod.Logger);
         }
